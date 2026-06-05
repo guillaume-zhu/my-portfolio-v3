d10dbbeedff11ce63f49ce6085f8e4a60d62055e
GuillaumeZhu <guillaume.zhu@outlook.com>
2026-06-05 18:00:56 +0200

    feat(manifesto): add manifesto section with smooth scrolling

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,30 @@
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
+import Lenis from "lenis"
+import "lenis/dist/lenis.css"
+
 import { createThreeHero } from "../../three/createThreeHero"
 
 gsap.registerPlugin(ScrollTrigger)
 
-const threeHero = await createThreeHero()
+const lenis = new Lenis({
+  anchors: true,
+})
 
-const heroFrame = document.querySelector(".hero-three__frame")
-console.log(heroFrame)
+lenis.on("scroll", ScrollTrigger.update)
 
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
+
+// ----------------------
 // Scene scroll animation
+// ----------------------
+const threeHero = await createThreeHero()
+const heroFrame = document.querySelector(".hero-three__frame")
+
 ScrollTrigger.create({
   trigger: ".hero-three",
   start: "top top",
@@ -33,8 +48,54 @@ gsap.to(".hero-three__frame", {
   scrollTrigger: {
     trigger: ".manifesto",
     start: "top bottom-=100",
-    end: "top bottom-=750",
+    end: "top bottom-=500",
     scrub: true,
     markers: false,
   },
 })
+
+// ----------------------
+// Manifesto
+// ----------------------
+document.fonts.ready.then(() => {
+  const text = document.querySelector(".manifesto .text")
+  wrapLettersInSpan(text)
+
+  const letters = document.querySelectorAll(".manifesto .letter")
+  const distance = text.clientWidth - document.body.clientWidth
+
+  const scrollTween = gsap.to(text, {
+    x: -distance,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".manifesto .container",
+      pin: true,
+      end: "+=" + distance,
+      markers: true,
+      scrub: true,
+    },
+  })
+
+  letters.forEach((letter) => {
+    gsap.from(letter, {
+      yPercent: (Math.random() - 0.5) * 400,
+      rotation: (Math.random() - 0.5) * 60,
+      ease: "elastic.out(1.2, 1)",
+      scrollTrigger: {
+        trigger: letter,
+        containerAnimation: scrollTween,
+        start: "left 100%",
+        end: "left 0%",
+        scrub: 0.5,
+      },
+    })
+  })
+})
+
+const wrapLettersInSpan = (element) => {
+  const text = element.textContent
+  element.innerHTML = text
+    .split("")
+    .map((char) => (char === " " ? "<span>&nbsp;</span>" : `<span class="letter">${char}</span>`))
+    .join("")
+}
