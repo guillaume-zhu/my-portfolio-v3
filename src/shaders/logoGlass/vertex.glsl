8f24d934a895fa65dc5c280ad9388d20df6f3316
GuillaumeZhu <guillaume.zhu@outlook.com>
2026-06-06 16:34:46 +0200

    feat(scene): sync silver logo with onbeforecompile and add click ripple

--- src/shaders/logoGlass/vertex.glsl
+++ src/shaders/logoGlass/vertex.glsl
@@ -1,11 +1,30 @@
 uniform float uSurfaceOffset;
 
+uniform float uTime;
+uniform float uHoverProgress;
+
+uniform float uClickProgress;
+uniform float uClickStrength;
+uniform float uClickWaveFrequency;
+uniform float uClickWaveSpeed;
+
 varying vec3 vNormal;
 varying vec3 vPosition;
 varying vec4 vScreenPosition;
 
 void main() {
-    vec3 displacedPosition = position + normal * uSurfaceOffset;
+    // Click wave effect
+    float clickEffect = uClickProgress * uHoverProgress;
+
+    float waveA = sin(position.y * uClickWaveFrequency + uTime * uClickWaveSpeed);
+    float waveB = sin(position.x * uClickWaveFrequency * 0.7 - uTime * uClickWaveSpeed * 0.6);
+
+    float fluidWave = (waveA + waveB) * 0.5;
+
+    float fluidOffset = fluidWave * uClickStrength * clickEffect;
+
+    // Displacement
+    vec3 displacedPosition = position + normal * (uSurfaceOffset + fluidOffset);
     
     vec4 modelPosition = modelMatrix * vec4(displacedPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
